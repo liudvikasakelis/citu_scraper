e3e11cf99cc202484c14d32a5115fc9db3f3628a
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-05-31 17:26:21 +0100

    made the universal scraper function

--- l1.py
+++ l1.py
@@ -54,3 +54,43 @@ def en_scraper(url):
         return(tree.xpath('//div[contains(text(), "Website")]/../../td[2]/a/@href')[0])
     except IndexError:
         return 0
+
+
+def scraper(url, site):
+    if site == 'en':
+        xpath_query_1 = '//div[@class="search_result"]//a/@href'
+        xpath_query_2 = '//div[contains(text(), "Website")]/../../td[2]/a/@href'
+        url_prefix = 'https://www.endole.co.uk'
+        search_prefix = '/search/?match=companies&search='        
+    elif site == 'co':
+        xpath_query_1 = '//a[@class="search-title-link"]/@href'
+        xpath_query_2 = '//strong[contains(text(), "Web")]/following-sibling::a/@href'
+        url_prefix = 'https://www.companiesintheuk.co.uk'
+        search_prefix = '/company/find?q='
+    else:
+        return 'Unknown site id'
+    
+    try:
+        page = requests.get(url_prefix + search_prefix + url)
+    except requests.exceptions.ConnectionError:
+        return 'Search results not received'
+           
+    tree = html.fromstring(page.content)
+    
+    try:
+        first = tree.xpath(xpath_query_1)[0]
+    except IndexError:
+        return '1st result not found in search results'
+    
+    try:
+        page = requests.get(url_prefix + first)
+    except requests.exceptions.ConnectionError:
+        return 'Company page not received'
+    
+    tree = html.fromstring(page.content)
+    
+    try:
+        return(tree.xpath(xpath_query_2)[0])
+    except IndexError:
+        return 'Website URL not found in company page'
+    
--- t.py
+++ t.py
@@ -4,8 +4,8 @@
 import l1
 import sys
 
-query = sys.argv[1]
+query = sys.argv[2]
+site = sys.argv[1]
 
-print(l1.st_scraper('https://www.companiesintheuk.co.uk/Company/Find?q=' + query))
+print(l1.scraper(query, site))
 
-print(l1.en_scraper('https://www.endole.co.uk/search/?match=companies&search=' + query))
