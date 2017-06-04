382e68f3737021597a4e03177702b31cf64c3ef3
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-06-04 10:52:25 +0100

    the old way

---
+++ t sqlite.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+
+import l1
+import sys
+import sqlite3
+import random
+import time
+
+
+if len(sys.argv) < 3:
+    sys.exit(2)
+
+db_path = sys.argv[2]
+site = sys.argv[1]
+
+proxy = ''
+
+ret = 0
+
+
+with sqlite3.connect(db_path) as conn:
+    c = conn.cursor()
+    while True:
+        time.sleep(random.gammavariate(1,0.1))
+        c.execute('SELECT * FROM website WHERE website_url = "0" OR website_url = ?;',
+                  tuple({'co', 'en'} - {site}))
+        all_names = c.fetchall()
+        if len(all_names) == 0:
+            break        
+        current_name, status = random.choice(all_names)
+        ret = l1.scraper(site=site, query=current_name)
+        
+        if ret in {-1, -2, -4}:
+            print(ret)
+            break
+        
+        if ret in {-3, -5}:
+            ret = site
+            if status != '0':
+                ret = "No website"
+                
+        c.execute('UPDATE website SET website_url = ? WHERE company_name = ?',
+                  [ret, current_name])
+        
+        conn.commit()
+            
+            
